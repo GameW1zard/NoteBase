8f69ce7310733b45c8d1094003387cf5e6dd2611
GameW1zard <rivenloya@gmail.com>
2023-04-22 15:45:57 -0500

    Update server.js

--- server.js
+++ server.js
@@ -31,14 +31,14 @@ app.post('/api/notes', function (req,res) {
     JSON.stringify(notes, null, 4),
     (err) => err ? console.error(err) : console.log("Write success!"))
 })
-async function deleteCreator () {
-for (var i = 0; i <= notes.length; i++){
-    let object = notes[i]
-    console.log(object.id)
-    app.delete(`/api/notes/${object.id}`, function (req, res){
-        console.log(req)
-    })
-}}
+// async function deleteCreator () {
+// for (var i = 0; i <= notes.length; i++){
+//     let object = notes[i]
+//     console.log(object.id)
+//     app.delete(`/api/notes/${object.id}`, function (req, res){
+//         console.log(req)
+//     })
+// }}
 //console.log(notes[1])
 
-app.listen(PORT, function () {console.log(`serving static assets at ${PORT}`), deleteCreator()})
+app.listen(PORT, function () {console.log(`serving static assets at ${PORT}`)})
